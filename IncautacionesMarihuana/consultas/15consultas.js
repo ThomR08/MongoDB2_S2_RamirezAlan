64c3faa015a83ad646155d9e8f567506a7c67894
Alan Ramirez (Cl Cjs) <thomasramirez0806@gmail.com>
2025-10-09 13:04:38 -0500

    feat :sparkles: Developing querys

--- IncautacionesMarihuana/consultas/15consultas.js
+++ IncautacionesMarihuana/consultas/15consultas.js
@@ -9,14 +9,16 @@ db.Municipio.aggregate([{
             $options: "i"
         }
     }
-}, {
+},
+{
     $lookup: {
         from: "Incautacion",
         localField: "_id",
         foreignField: "municipio_id",
         as: "inca"
     }
-},{
+},
+{
     $project: {
         _id: 0,
         nombreMunicipio: "$nombre",
@@ -25,8 +27,99 @@ db.Municipio.aggregate([{
             $sum: "$inca.cantidadEnKg"
         }
     }
-},{
+},
+{
     $sort: {
         cantidadIncautada: -1
     }
 }]);
+
+// 2.
+
+db.Municipio.aggregate([{
+    $match: {
+        nombre: {
+            $regex: "al$",
+            $options: "i"
+        }
+    }
+},
+{
+    $lookup: {
+        from: "Incautacion",
+        localField: "_id",
+        foreignField: "municipio_id",
+        as: "inca"
+    }
+},
+{
+    $project: {
+        _id: 0,
+        nombreMunicipio: "$nombre",
+        codigoMunicipio: 1,
+        cantidadIncautada: {
+            $sum: "$inca.cantidadEnKg"
+        }
+    }
+},
+{
+    $sort: {
+        cantidadIncautada: -1
+    }
+},
+{
+    $limit: 5
+}]);
+
+// 3.
+
+db.Municipio.aggregate([{
+    $match: {
+        nombre: {
+            $regex: "z",
+            $options: "i"
+        }
+    }
+},
+{
+    $lookup: {
+        from: "Incautacion",
+        localField: "_id",
+        foreignField: "municipio_id",
+        as: "inca"
+    }
+},
+{
+    $unwind: "$inca"
+},
+{
+    $group: {
+        _id: {
+            ano: {
+                $year: "$inca.fecha"
+            },
+            municipioId: "$_id"
+        },
+        Municipio: {
+            $first: "$nombre"
+        },
+        totalIncaKg: {
+            $sum: "$inca.cantidadEnKg"
+        }
+    }
+},
+{
+    $sort: {
+        "_id.ano": -1,
+        totalIncaKg: -1
+    }
+},
+{
+    $project: {
+        _id: 0,
+        Municipio: "$nombre",
+        codigoMunicipio: "$codigoMunicipio",
+        ano: "$_id.ano",
+        totalIncaKg: "$totalIncaAno"
+    }
+}])
