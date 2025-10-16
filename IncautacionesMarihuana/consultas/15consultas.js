e6b705c6d9715895100a93e8f5aa87ffd5c76d05
ThomR08 <thomasramirez0806@gmail.com>
2025-10-16 08:05:56 -0500

    feat :sparkles: Developing querys

--- IncautacionesMarihuana/consultas/15consultas.js
+++ IncautacionesMarihuana/consultas/15consultas.js
@@ -1,6 +1,6 @@
 use IncautacionesMarihuana;
 
-// 1.
+// 1. ¿Cuántos municipios comienzan con "La" y cuál es la cantidad total incautada en ellos?
 
 db.Municipio.aggregate([{
     $match: {
@@ -34,7 +34,7 @@ db.Municipio.aggregate([{
     }
 }]);
 
-// 2.
+// 2. Top 5 departamentos donde los municipios terminan en "al" y la cantidad incautada.
 
 db.Municipio.aggregate([{
     $match: {
@@ -71,7 +71,7 @@ db.Municipio.aggregate([{
     $limit: 5
 }]);
 
-// 3.
+// 3. Por cada año, muestra los 3 municipios con más incautaciones, pero únicamente si su nombre contiene la letra "z".
 
 db.Municipio.aggregate([{
     $match: {
@@ -123,3 +123,124 @@ db.Municipio.aggregate([{
         totalIncaKg: "$totalIncaAno"
     }
 }])
+
+// 4. ¿Qué unidad de medida aparece en registros de municipios que empiecen por "Santa"?
+
+// El ejercicio no aplica para este sistema de información, todas las incautaciones están normalizadas a kilogramos
+
+// 5. ¿Cuál es la cantidad promedio de incautaciones en los municipios cuyo nombre contiene "Valle"?
+
+db.Municipio.aggregate([{
+    $match: {
+        nombre: {
+            $regex: "Valle",
+            $options: "i"
+        }
+    }
+},
+{
+    $lookup: {
+        from: "Incautacion",
+        localField: "_id",
+        foreignField: "municipio_id",
+        as: "inca"
+    }
+},
+{
+    $project: {
+        _id: 0,
+        nombreMunicipio: "$nombre",
+        codigoMunicipio: 1,
+        promedioIncautacion: {
+            $avg: "$inca.cantidadEnKg"
+        }
+    }
+},
+{
+    $sort: {
+        promedioIncautacion: -1
+    }
+}]);
+
+// 6. ¿Cuántos registros hay en municipios cuyo nombre contenga exactamente 7 letras?
+
+db.Municipio.aggregate([{
+    $match: {
+        "nombre": {
+            $regex: "^.{7}$",
+            $options: "i"
+        }
+    }
+},
+{
+    $lookup: {
+        from: "Incautacion",
+        localField: "_id",
+        foreignField: "municipio_id",
+        as: "inca"
+    }
+},
+{
+    $unwind: "$inca"
+},
+{
+    $group: {
+        _id: "$inca.municipio_id",
+        cantidadIncautaciones: {
+            $sum: 1
+        },
+        nombreMunicipio: "$nombre",
+        codigoMunicipio: 1
+    }
+},
+{
+    $project: {
+        _id: 0,
+        nombreMunicipio: 1,
+        codigoMunicipio: 1,
+        cantidadIncautaciones: 1
+    }
+}]);
+
+// 7. ¿Cuáles son los 10 municipios con mayor cantidad incautada en 2020?
+
+db.Incautacion.aggregate([{
+    $group: {
+        _id: {
+            municipio_id: "$municipio_id",
+            ano: {
+                $year: "$fecha"
+            }
+        },
+        totalIncaKg: {
+            $sum: "$cantidadEnKg"
+        }
+    }
+},{
+    $match: {
+        "_id.ano": 2020
+    }
+},{
+    $sort: {
+        totalIncaKg: -1
+    }
+},{
+    $limit: 10
+},{
+    $lookup: {
+        from: "Municipio",
+        localField: "_id.municipio_id",
+        foreignField: "_id",
+        as: "Muni"
+    }
+},{
+    $unwind: "$Muni"
+},{
+    $project: {
+        _id: 0,
+        nombreMunicipio: "$Muni.nombre",
+        totalIncaKg: 1
+    }
+}])
+
+// 8. Por cada departamento, muestra el municipio con más cantidad incautada.
